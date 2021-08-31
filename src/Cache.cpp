070fb9ae4b7b7f7059a24f55048db33f016b2d03
Stevan Dupor <smdupor@gmail.com>
2021-08-31 08:22:05 -0400

    Code style cleanup

--- src/Cache.cpp
+++ src/Cache.cpp
@@ -17,23 +17,23 @@ Cache::Cache(cache_params params, uint8_t level) {
    this->params = params;
    this->level = level;
    this->main_memory = false;
-   reads=0, read_misses=0, read_hits=0, writes=0, write_misses=0, write_hits=0, vc_swaps=0, write_backs=0, vc_swap_requests=0;
+   reads = 0, read_misses = 0, read_hits = 0, writes = 0, write_misses = 0, write_hits = 0, vc_swaps = 0, write_backs = 0, vc_swap_requests = 0;
 
-   switch(level) {
+   switch (level) {
       case 0xff: // This is a main  memory
          this->main_memory = true;
          return;
       case 0x01: // This is an L1 cache
-         local_size =  params.l1_size;
+         local_size = params.l1_size;
          local_assoc = params.l1_assoc;
          initialize_cache_memory();
          ++level;
-         if(params.l2_size == 0)
+         if (params.l2_size == 0)
             level = 0xff; // There is no L2 cache so the next level is Main Memory
          next_level = new Cache(params, level);
          return;
       case 0x02: // This is an L2 cache
-         local_size =  params.l2_size;
+         local_size = params.l2_size;
          local_assoc = params.l2_assoc;
          initialize_cache_memory();
          level = 0xff; // Force next level to main memory
@@ -44,8 +44,8 @@ Cache::Cache(cache_params params, uint8_t level) {
 
 inline void Cache::initialize_cache_memory() {
    block_size = (uint8_t) params.block_size;
-   size_t num_sets = local_size / (local_assoc*block_size);
-   for(size_t i =0; i<num_sets; ++i) {
+   size_t num_sets = local_size / (local_assoc * block_size);
+   for (size_t i = 0; i < num_sets; ++i) {
       sets.emplace_back(Set(local_assoc));
    }
    index_length = log2(num_sets);
@@ -56,8 +56,8 @@ inline void Cache::initialize_cache_memory() {
 // Stub destructor
 Cache::~Cache() = default;
 
-void Cache::read(unsigned long &addr){
-   if(this->main_memory) {
+void Cache::read(unsigned long &addr) {
+   if (this->main_memory) {
       ++this->reads;
       return;
    }
@@ -65,14 +65,14 @@ void Cache::read(unsigned long &addr){
    // Separate tag, index, block offset
    uint_fast32_t tag = addr >> (index_length + block_length);
    uint_fast32_t index = addr - (tag << (index_length + block_length));
-   index  = index >> block_length;
+   index = index >> block_length;
 
    // Search the set at the calculated index for the requested block
    auto block = std::find_if(sets[index].blocks.begin(), sets[index].blocks.end(), [&](Block b) {
       return b.valid && b.tag == tag;
    });
 
-   if(block == sets[index].blocks.end()) {
+   if (block == sets[index].blocks.end()) {
       //MISS
       ++read_misses;
 
@@ -85,7 +85,7 @@ void Cache::read(unsigned long &addr){
       });
 
       // If dirty, writeback
-      if(oldest_block->dirty) {
+      if (oldest_block->dirty) {
          ++write_backs;
          next_level->write(addr);
       }
@@ -97,15 +97,14 @@ void Cache::read(unsigned long &addr){
 
       // Traverse and update recency
 
-         for (Block &traversal_block : sets[index].blocks)
-             ++traversal_block.recency;
+      for (Block &traversal_block : sets[index].blocks)
+         ++traversal_block.recency;
       oldest_block->recency = 0;
-   }
-   else {
+   } else {
       ++read_hits;
 
       //If the recency hierarchy has changed, traverse the set and update recencies
-      if(block->recency != 0) {
+      if (block->recency != 0) {
          for (Block &traversal_block : sets[index].blocks)
             if (traversal_block.recency < block->recency)
                ++traversal_block.recency;
@@ -116,7 +115,7 @@ void Cache::read(unsigned long &addr){
 }
 
 void Cache::write(unsigned long &addr) {
-   if(this->main_memory) {
+   if (this->main_memory) {
       ++this->writes;
       return;
    }
@@ -124,14 +123,14 @@ void Cache::write(unsigned long &addr) {
    // Separate tag, index, block offset
    uint_fast32_t tag = addr >> (index_length + block_length);
    uint_fast32_t index = addr - (tag << (index_length + block_length));
-   index  = index >> block_length;
+   index = index >> block_length;
 
    // Search the set at the calculated index for the requested block
    auto block = std::find_if(sets[index].blocks.begin(), sets[index].blocks.end(), [&](Block b) {
       return b.valid && b.tag == tag;
    });
 
-   if(block == sets[index].blocks.end()) {
+   if (block == sets[index].blocks.end()) {
       //MISS
       ++write_misses;
 
@@ -145,7 +144,7 @@ void Cache::write(unsigned long &addr) {
       });
 
       // If dirty, writeback
-      if(oldest_block->dirty) {
+      if (oldest_block->dirty) {
          ++write_backs;
          next_level->write(addr);
       }
@@ -162,14 +161,13 @@ void Cache::write(unsigned long &addr) {
       for (Block &traversal_block : sets[index].blocks)
          ++traversal_block.recency;
       oldest_block->recency = 0;
-   }
-   else //HIT
+   } else //HIT
    {
       block->dirty = true;
       ++write_hits;
 
       //If the recency hierarchy has changed, traverse the set and update recencies
-      if(block->recency != 0) {
+      if (block->recency != 0) {
          for (Block &traversal_block : sets[index].blocks)
             if (traversal_block.recency < block->recency)
                ++traversal_block.recency;
@@ -180,13 +178,13 @@ void Cache::write(unsigned long &addr) {
 }
 
 void Cache::contents_report() {
-   if(this->main_memory)
+   if (this->main_memory)
       return;
-   std::cout << "===== L" << std::to_string(this->level) <<" contents =====\n";
-   for(size_t i = 0; i<sets.size(); ++i)
+   std::cout << "===== L" << std::to_string(this->level) << " contents =====\n";
+   for (size_t i = 0; i < sets.size(); ++i)
       cache_line_report(i);
    std::cout << "\n";
-   this ->next_level->contents_report();
+   this->next_level->contents_report();
 }
 
 
@@ -203,7 +201,7 @@ void Cache::cache_line_report(uint8_t set_num) {
    std::sort(temp_set.begin(), temp_set.end());
 
    // Traverse the set and Convert the contents to a string
-   for(Block &b : temp_set){
+   for (Block &b : temp_set) {
       output += "  ";
       std::stringstream ss;
       ss << std::hex << b.tag;
@@ -222,7 +220,7 @@ void Cache::cache_line_report(uint8_t set_num) {
 
 void Cache::statistics_report() {
 
-   if(this->level == 1) {
+   if (this->level == 1) {
       L1_stats_report();
       next_level->statistics_report();
       return;
@@ -234,21 +232,21 @@ void Cache::L1_stats_report() {
    std::string output = "===== Simulation results =====\n";
    output += "a. number of L1 reads:                ";
    cat_padded(&output, this->reads);
-   output+= "b. number of L1 read misses:          ";
+   output += "b. number of L1 read misses:          ";
    cat_padded(&output, this->read_misses);
-   output+= "c. number of L1 writes:               ";
+   output += "c. number of L1 writes:               ";
    cat_padded(&output, this->writes);
-   output+= "d. number of L1 write misses:         ";
+   output += "d. number of L1 write misses:         ";
    cat_padded(&output, this->write_misses);
-   output+= "e. number of swap requests:           ";
+   output += "e. number of swap requests:           ";
    cat_padded(&output, this->vc_swap_requests);
-   output+= "f. swap request rate:                 ";
+   output += "f. swap request rate:                 ";
    cat_padded(&output, 0.0); // TODO swap request rate
-   output+= "g. number of swaps:                   ";
+   output += "g. number of swaps:                   ";
    cat_padded(&output, this->vc_swaps);
-   output+= "h. combined L1+VC miss rate:          ";
+   output += "h. combined L1+VC miss rate:          ";
    cat_padded(&output, 0.0); // TODO l1+vc miss rate
-   output+= "i. number writebacks from L1/VC:      ";
+   output += "i. number writebacks from L1/VC:      ";
    cat_padded(&output, this->write_backs);
 
    std::cout << output;
@@ -257,7 +255,7 @@ void Cache::L1_stats_report() {
 void Cache::L2_stats_report() {
    std::string output = "";
 
-   if(level == 2) {
+   if (level == 2) {
       output += "j. number of L2 reads:                ";
       cat_padded(&output, this->reads);
       output += "k. number of L2 read misses:          ";
@@ -272,9 +270,7 @@ void Cache::L2_stats_report() {
       cat_padded(&output, this->write_backs);
       output += "p. total memory traffic:              ";
       cat_padded(&output, (this->next_level->reads + this->next_level->writes));
-   }
-   else
-   {
+   } else {
       output += "j. number of L2 reads:                ";
       cat_padded(&output, (uint_fast32_t) 0);
       output += "k. number of L2 read misses:          ";
@@ -286,7 +282,7 @@ void Cache::L2_stats_report() {
       output += "n. L2 miss rate:                      ";
       cat_padded(&output, 0.0); //Todo miss rate
       output += "o. number of writebacks from L2:      ";
-      cat_padded(&output, (uint_fast32_t)  0);
+      cat_padded(&output, (uint_fast32_t) 0);
       output += "p. total memory traffic:              ";
       cat_padded(&output, (this->reads + this->writes));
    }
@@ -296,7 +292,7 @@ void Cache::L2_stats_report() {
 // Attach a numeric value to the end of a string with space padding
 void Cache::cat_padded(std::string *str, uint_fast32_t n) {
    std::string value = std::to_string(n);
-   while(value.length() < 12)
+   while (value.length() < 12)
       value = " " + value;
    value += "\n";
    *str += value;
@@ -304,8 +300,8 @@ void Cache::cat_padded(std::string *str, uint_fast32_t n) {
 
 // Attach a numeric value to the end of a string with space padding
 void Cache::cat_padded(std::string *str, double n) {
-   std::string value = std::to_string(n).substr(0,6);
-   while(value.length() < 12)
+   std::string value = std::to_string(n).substr(0, 6);
+   while (value.length() < 12)
       value = " " + value;
    value += "\n";
    *str += value;
